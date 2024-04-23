3766f866cbad99fb7c68237a8eb16f70c1ee5d36
Feng1yi <Feng1yi@foxmail.com>
2024-04-24 00:19:24 +0800

    templates

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -5,4 +5,10 @@ from django.http import HttpRequest
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('ok')
+    # return HttpResponse('ok')
+
+    # 模拟数据查询
+    context = {
+        'name': '书籍列表'
+    }
+    return render(request, 'book/index.html', context=context)
