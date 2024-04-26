797acb2e37576909a641a3fd4fa5610e9aa30ce9
fengyi99 <feng1yi@foxmail.com>
2024-04-27 00:29:20 +0800

    mysql

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -12,3 +12,21 @@ def index(request):
         'name': '书籍列表'
     }
     return render(request, 'book/index.html', context=context)
+
+# 数据的增删改查
+# 在pycharm的终端中的shell中执行
+# python manage.py shell
+# 1. 增加数据
+# 方式一：save
+from book.models import BookInfo, PeopleInfo
+book = BookInfo(
+    name='Python入门',
+    pub_date = '2000-01-01'
+)
+book # 查询是否添加成功
+book.save()
+# 方式二：create，通过模型类.objects.create()保存。
+PeopleInfo.objects.create(
+    name='django',
+    book = book
+)
